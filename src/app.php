6c4525f52ecbc82d1fc6c0c76809d0cecf4fafc1
Christoph Ebert <ebert86@gmail.com>
2013-02-22 17:26:20 +0100

    Render view through twig

--- src/app.php
+++ src/app.php
@@ -1,10 +1,8 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
-
-$app = new Silex\Application();
 $app->register(new Silex\Provider\DoctrineServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => array(__DIR__ . '/views')
+));
 
-$app['debug'] = true;
-
-$app->run();
+return $app;
--- web/index.php
+++ web/index.php
@@ -1,29 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Friend Battle</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="css/style.css"/>
-</head>
-<body>
-<input type="hidden" id="fb_locale" value="en_US" />
-<input type="hidden" id="fb_app_id" value="540154819351099" />
-<input type="hidden" id="fb_appNamespace" value="liipfriendbattle" />
-<div id="fb-root"></div>
-<div id="welcome-msg">You are not logged in</div>
-<div id="navigation" class="hidden">
-    <ul>
-        <li class="first"><a href="#" onclick="FBHelper.loadActivityStream();">Activity stream</a></li>
-        <li><a href="#" onclick="FBHelper.inviteFriends();">Invite friends</a></li>
-    </ul>
-</div>
-<div id="content"></div>
+<?php
 
-<script async src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script src="js/fb.helper.js"></script>
-<script src="js/fb.helper.activitystream.js"></script>
-<script src="js/fb.helper.invite.js"></script>
-<script src="js/fb.init.js"></script>
-<script src="js/fb.sdk.js"></script>
-</body>
-</html>
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+require_once __DIR__.'/../vendor/autoload.php';
+$app = new Silex\Application();
+
+require __DIR__.'/../src/config.php';
+require __DIR__.'/../src/app.php';
+
+$app->match('/', function() use ($app) {
+    return $app['twig']->render('index.html.twig');
+});
+
+$app['debug'] = true;
+$app->run();
